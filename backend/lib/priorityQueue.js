29ef4a6c81dfee4ca3f00642847012bb80b0a3b0
dvsqwx <dvsqwx@proton.me>
2026-03-10 14:59:03 +0200

    Add BiDirectionalPriorityQueue class
    Implemented a Bi-Directional Priority Queue class with enqueue method.

--- backend/lib/priorityQueue.js
+++ backend/lib/priorityQueue.js
@@ -1,1 +1,21 @@
+'use strict'
 
+// Task 4 - Bi-Directional Priority Queue
+
+export class BiDirectionalPriorityQueue {
+
+    constructor() {
+        this.items = []
+        this.counter = 0  // tracks insertion order
+    }
+
+    enqueue(item, priority = 0) {
+        this.items.push({
+            item,
+            priority,
+            order: this.counter,
+        })
+        this.counter++
+    }
+
+}
