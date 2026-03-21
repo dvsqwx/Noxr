c408c1523f0496f7d93011aa6932b41df6d8d288
dvsqwx <dvsqwx@proton.me>
2026-03-21 16:52:23 +0200

    Update streams.js

--- backend/lib/streams.js
+++ backend/lib/streams.js
@@ -1,1 +1,18 @@
+'use strict'
 
+// task6
+
+export async function* streamFromArray(arr) {
+    for (const item of arr) {
+        yield await item
+    }
+}
+
+export async function* streamFilter(stream, predicate) {
+    for await (const item of stream) {
+        const result = predicate(item)
+        if (result == true) {
+            yield await item
+        }
+    }
+}
