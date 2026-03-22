1d4e1c1ac1bb2d57fe7f212affc6679e9d69df96
dvsqwx <dvsqwx@proton.me>
2026-03-23 01:42:35 +0200

    Update asyncFilter.js

--- backend/lib/asyncFilter.js
+++ backend/lib/asyncFilter.js
@@ -9,3 +9,20 @@ export function asyncFilterCallback(array, predicate, callback) {
         callback(null, [])
         return
     }
+
+    array.forEach(function(item, i) {
+        Promise.resolve(predicate(item)).then(function(ok) {
+            if(ok) {
+                res.push({item, i})
+            }
+        }).catch(function(e) {
+            callback(e)
+        }).finally(function() {
+            n--
+            if(n == 0) {
+                res.sort(function(a,b) { return a.i - b.i })
+                callback(null, res.map(function(x) { return x.item }))
+            }
+        })
+    })
+}
