945547da26ffbedd6e6e207f69845f5157433940
dvsqwx <dvsqwx@proton.me>
2026-03-23 17:15:20 +0200

    Update priorityQueue.js

--- backend/lib/priorityQueue.js
+++ backend/lib/priorityQueue.js
@@ -1,1 +1,18 @@
-'use strict'
+'use strict';
+
+//task 4 bi directional queue
+export class BiDirectionalPriorityQueue {
+    constructor() {
+        this.items = []
+        this.count = 0
+    }
+
+    enqueue(item, priority = 0) {
+        const newItem = {
+            item,
+            priority,
+            count: this.count
+        }
+        this.items.push(newItem)
+        this.count++
+    }
