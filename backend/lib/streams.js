99f23b2c698faa451ddac64caa3e39a16bb6d12d
dvsqwx <dvsqwx@proton.me>
2026-03-27 17:02:46 +0200

    fix

--- backend/lib/streams.js
+++ backend/lib/streams.js
@@ -8,11 +8,3 @@ export async function* streamFromArray(arr) {
     }
 }
 
-export async function* streamFilter(stream, predicate) {
-    for await (const item of stream) {
-        const result = predicate(item)
-        if (result == true) {
-            yield await item
-        }
-    }
-}
