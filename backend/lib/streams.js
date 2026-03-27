120cbcce66da998eb80393780e048993085a9226
dvsqwx <dvsqwx@proton.me>
2026-03-27 17:05:57 +0200

    Update streams.js

--- backend/lib/streams.js
+++ backend/lib/streams.js
@@ -8,3 +8,22 @@ export async function* streamFromArray(arr) {
     }
 }
 
+export async function* streamJsonlFile(filepath) {
+    const fs = await import('fs')
+    const readline = await import('readline')
+
+    const fileStream = fs.createReadStream(filepath)
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    })
+
+    for await (const line of rl) {
+        if(line.trim() === '') continue
+        try {
+            yield JSON.parse(line)
+        } catch(e) {
+            console.error('bad line:', line)
+        }
+    }
+}
