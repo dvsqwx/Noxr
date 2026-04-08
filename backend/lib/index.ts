efc696335f2dc6d8c10fc7e075fdaf77845e8c8b
dvsqwx <dvsqwx@proton.me>
2026-04-08 13:15:46 +0300

    Export multiple modules from index.ts

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,1 +1,47 @@
+export { newsFeedGenerator, timeoutIterator } from './generator.js'
 
+export { memoize } from './memoize.js'
+
+export {
+    BiDirectionalPriorityQueue
+} from './priorityQueue.js'
+
+export {
+    asyncFilterCallback,
+    asyncFilterSync,
+    isHighPriority,
+    isMediumPriority,
+    isTech,
+    isCrypto,
+    isMemes
+} from './asyncFilter.js'
+
+export {
+    streamFromArray,
+    streamFilter,
+    streamMap,
+    streamTake,
+    collectStream,
+    streamJsonlFile
+} from './streams.js'
+
+export {
+    EventEmitter,
+    EVENTS,
+    getEmitter
+} from './eventEmitter.js'
+
+export {
+    createLogger,
+    createFileLogger,
+    withLogging,
+    withLoggingAsync
+} from './logger.js'
+
+export {
+    ApiKeyStrategy,
+    BearerTokenStrategy,
+    OAuthStrategy,
+    RateLimiter,
+    AuthProxy
+} from './authProxy.js'
